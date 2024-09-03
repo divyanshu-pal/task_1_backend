15daaca5c45d63e5d978e072cad9748809ef7aa9
Divyanshu Pal <86774540+divyanshu-pal@users.noreply.github.com>
2024-09-03 15:40:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const dotenv = require('dotenv');
 const router = require('./route');
 const connectDB = require('./database')
-
+app.use(cors());
 app.use(express.json());
 dotenv.config();
 connectDB();
